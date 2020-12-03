595478d757ee1e64ec2580c768c08e0f10e52d0d
joaosenger <74719360+joaosenger@users.noreply.github.com>
2020-12-03 08:29:20 -0300

    Create 01-tipagemFraca.js

---
+++ 01-fundamentos/01-tipagemFraca.js
@@ -0,0 +1,18 @@
+/*
+É organizada em sentenças de código e blocos de código, 
+as sentenças de código podem ou não terminar com ponto
+e vírgular
+*/
+
+//linha de código
+console.log("Sentença de código")
+
+//blocos de código
+{
+    {  
+        console.log("Bloco de código")
+        console.log("Olá ")
+        console.log("Mundo!")
+    }
+}
+     
